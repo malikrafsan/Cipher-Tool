77fd95b1a3056dd062bf299fae4cc1ce689f16de
Malik Rafsanjani <pro.malikakbar2357@gmail.com>
2023-01-25 17:53:21 +0700

    feat: change a tag to div tag and remove comments

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,25 @@
 import Link from "next/link";
 
-// a) Vigenere Cipher standard (26 huruf alfabet)
-// b) Varian Vigenere Cipher (26 huruf alfabet): Auto-key Vigenere Cipher
-// c) Extended Vigenere Cipher (256 karakter ASCII)
-// d) Affine Cipher
-// e) Playfair Cipher (26 huruf alfabet)
-// f) Hill Cipher
-
 const Home = () => {
   return (
     <div>
       <Link href="/vigenere-cipher">
-        <a>Vigenere Cipher</a>
+        <div>Vigenere Cipher</div>
       </Link>
       <Link href="/varian-vigenere-cipher">
-        <a>Varian Vigenere Cipher</a>
+        <div>Varian Vigenere Cipher</div>
       </Link>
       <Link href="/extended-vigenere-cipher">
-        <a>Extended Vigenere Cipher</a>
+        <div>Extended Vigenere Cipher</div>
       </Link>
       <Link href="/affine-cipher">
-        <a>Affine Cipher</a>
+        <div>Affine Cipher</div>
       </Link>
       <Link href="/playfair-cipher">
-        <a>Playfair Cipher</a>
+        <div>Playfair Cipher</div>
       </Link>
       <Link href="/hill-cipher">
-        <a>Hill Cipher</a>
+        <div>Hill Cipher</div>
       </Link>
     </div>
   );
